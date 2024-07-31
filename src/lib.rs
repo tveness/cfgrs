d2de8b794722d97673ee443638c46b8dd64e2efc
Thomas Veness <veness@protonmail.com>
2024-07-31 20:29:45 +0100

    Update library documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! This is the library documentation for code which is used in the binary `cfgrs`, which is likely
+//! what you're looking for.
+//! This can be found at https://github.com/tveness/cfgrs/releases
+//! or alternatively can be installed with `cargo install cfgrs`.
+//!
+//!
+//! cfgrs is a small CLI helper tool for converting between different configuration formats.
+//! The current formats supported are:
+//! * hcl
+//! * json
+//! * toml
+//! * yaml
+//!
+//! These formats are not completely interchangeable, and as such if they cannot
+//! be converted an error is currently raised.
+//!
+//! cfgrs may be used in the following way to convert between formats:
+//! ```bash
+//! cat Cargo.toml|cfgrs -o yaml
+//! ```
+//! which will then display a Cargo.toml file in its yaml representation (if this is possible)
+
 use serde::Serialize;
 use std::str::FromStr;
 
@@ -20,6 +42,7 @@ pub enum ParsedInput {
     Hcl(hcl::Body),
 }
 
+/// Error type for parsing input
 #[derive(Debug)]
 pub struct ParseConfigError {}
 
